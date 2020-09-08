ae091a0aa9ac88375e58db930d9912384f1f0da7
Ronan <ronandefreitas@gmail.com>
2020-09-08 19:56:29 -0300

    Alteração da ProductFactory.php

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,7 +9,7 @@ $factory->define(Product::class, function (Faker $faker) {
     return [
         'name'=> $faker->streetName,
         'description' => $faker->text(100),
-        'price' => $faker->randomFloat(0,100),
+        'price' => $faker->randomFloat(0,100, 500),
         'img_src' => $faker->imageUrl($width = 200, $height = 200)
     ];
 });
